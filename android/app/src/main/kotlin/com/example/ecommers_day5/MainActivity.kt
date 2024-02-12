85dc0c0a3234087f8e37c348c6236d084385ada7
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-02-12 11:38:36 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ecommers_day5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ecommers_day5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:ecommers_day5/stack.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return stack();
+  }
+}
